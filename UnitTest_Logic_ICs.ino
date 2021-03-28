0cf0444397f3a8d1376a08c017e9d204c6315e8c
Ben Anderman <ben@happyspork.com>
2021-03-27 18:11:52 -0700

    Add 139 and 157 support (dual decoders, and quad 2-to-1 selectors)

--- UnitTest_Logic_ICs.ino
+++ UnitTest_Logic_ICs.ino
@@ -1,13 +1,14 @@
 // Which type of chip to test, with leading 0 removed (the 0 would indicate an octal number)
-#define CHIP 00
+#define CHIP 0
 // Whether to continue testing if one set of inputs has the wrong output
 #define STOP_AFTER_FAIL true
 
-// On all chips, Arduino pins 2-7 should be connected to the chip's pins 1-6,
-// and Arduino pins 8-13 to chip pins 13-8. The second set is in reverse order,
-// because the gate input/output order is reversed on the right side of the chip.
-// And of course, connect chip pin 7 to GND, and chip pin 14 to 5V.
+// On all chips, connect wires from top to bottom on the left, and top to bottom
+// on the right (on DIP-14 chips that means 1-6, 13-8, on DIP-16, 1-7, 15-9).
+// On the Arduino, those connect to pins 2-13, A0-A1.
+// And of course, connect bottom left of the chip to GND, and top right to 5V.
 
+// DIP-14 chips
 #if CHIP == 30
   const int outputPins[] = {2, 3, 4, 5, 6, 7, 9, 10};
   const int inputPins[] = {13};
@@ -21,6 +22,16 @@
 #elif CHIP == 4
   const int outputPins[] = {2, 4, 6, 8, 10, 12};
   const int inputPins[] = {3, 5, 7, 9, 11, 13};
+
+// DIP-16 chips
+#elif CHIP == 139
+  // Pins are out of order to make logic simpler below
+  const int outputPins[] = {2, 4, 3, 9, 11, 10};
+  const int inputPins[] = {8, 7, 6, 5, A1, A0, 13, 12};
+#elif CHIP == 157
+  const int outputPins[] = {9, 2, 3, 4, 6, 7, 10, 11, 13, A0};
+  const int inputPins[] = {5, 8, 12, A1};
+
 #else
   #error "Invalid CHIP"
 #endif
@@ -72,12 +83,8 @@ void loop() {
 }
 
 int getExpected(int value) {
-  // Single gate chip (8-input AND)
-  #if CHIP == 30
-    return (value == 255) ? 1 : 0;
-
   // Quad gate chips
-  #elif CHIP == 0 || CHIP == 2 || CHIP == 8 || CHIP == 32 || CHIP == 86
+  #if CHIP == 0 || CHIP == 2 || CHIP == 8 || CHIP == 32 || CHIP == 86
     int result = 0;
     for (int i = 3; i >= 0; i--) {
       bool v1 = value & (1 << (i * 2));
@@ -101,6 +108,32 @@ int getExpected(int value) {
   #elif CHIP == 4
     return ~value & 0b111111;
     
+  // Single gate chip (8-input AND)
+  #elif CHIP == 30
+    return (value == 255) ? 1 : 0;
+
+  // Dual decoder
+  #elif CHIP == 139
+  int v1 = (value >> 3) & 0b11;
+  int v2 = value & 0b11;
+  bool en1 = value & 0b100000;
+  bool en2 = value & 0b000100;
+  // Enable pins are inverted
+  int r1 = en1 ? 0 : 1 << v1;
+  int r2 = en2 ? 0 : 1 << v2;
+  // Outputs are inverted, but we don't want to invert the rest of the int's bits
+  return ~((r1 << 4) | r2) & 0xFF;
+
+  // Quad 2-line to 1-line data selectors / multiplexers
+  #elif CHIP == 157
+  bool g = value & (1 << 9);
+  bool ab = value & (1 << 8);
+  bool v1 = value & (1 << (7 - ab));
+  bool v2 = value & (1 << (5 - ab));
+  bool v3 = value & (1 << (3 - ab));
+  bool v4 = value & (1 << (1 - ab));
+  return g ? 0 : (v1 << 3) | (v2 << 2) | (v3 << 1) | v4;
+  
   #else
     #error "Invalid CHIP"
   #endif
